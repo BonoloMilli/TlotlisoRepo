523bb7e60451fd53634c4d9728292840809a3308
Tlotliso Mpeoa <Tlotliso2062@gmail.com>
2024-04-24 15:39:21 +0200

    added images

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("offcanvas");
+    sidebar.classList.toggle("show");
+}
+
+function closeSidebar() {
+    var sidebar = document.getElementById("offcanvas");
+    sidebar.classList.remove("show");
+}
